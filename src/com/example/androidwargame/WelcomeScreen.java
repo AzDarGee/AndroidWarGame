23fce1ce14b67b4353dafd22fbd8debca7a770ad
AzDarGee <ashishdarji88@gmail.com>
2013-11-19 19:33:43 -0500

    Update welcome screen

--- src/com/example/androidwargame/WelcomeScreen.java
+++ src/com/example/androidwargame/WelcomeScreen.java
@@ -7,44 +7,67 @@ import android.os.Bundle;
 
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+public class WelcomeScreen extends Activity {
 
-public class WelcomeScreen extends Activity{
-	
 	public final static String EXTRA_MESSAGE = "com.android.androidwargame.MESSAGE";
 	private final static String TAG = "WelcomeScreen";
-	
-	 @Override
-     protected void onCreate(Bundle savedInstanceState) { 
-             super.onCreate(savedInstanceState);
-             setContentView(R.layout.welcome_screen);      
-	 }
-	 
-	 public void proceed(View view) {
-		 
-		 switch(view.getId()) //get the id which is an int
-		    {
-		     case R.id.button1 : //if its button1 that is clicked
-		         Intent i= new Intent(this,MainActivity.class);
-		         i.putExtra(EXTRA_MESSAGE, "1");
-		         Log.d(TAG, "Chose Button 1 --> ASH");
-		         startActivity(i);
-		        // use intents to pass information to secondActivity and display the image there
-		      break;
-		      case R.id.button2 :
-		    	  Intent in= new Intent(this,MainActivity.class);
-			      in.putExtra(EXTRA_MESSAGE, "2");
-			      Log.d(TAG, "Chose Button 2 --> JOSH");
-			      startActivity(in);
-		      break;
-		    }
+	private EditText server;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome_screen);
+		server = (EditText) findViewById(R.id.editText1);
+	}
+
+	public void proceed(View view) {
+
+		String serverMSG = server.getText().toString();
+		Intent i = new Intent(this, MainActivity.class);
 		
+		switch (view.getId()) {
+
+		case (R.id.button1): // if its button1 that is clicked
+		{
+			if (serverMSG == "") {
+				Toast.makeText(WelcomeScreen.this, "Enter Server Address 2!",
+						Toast.LENGTH_LONG).show();
+				
+			} else if(serverMSG != "") {
 		
-		 
-	 }
-	 
-	 
-	 
+				i.putExtra(EXTRA_MESSAGE, "1");
+				i.putExtra(EXTRA_MESSAGE, serverMSG);
+				Log.d(TAG, "Chose Button 1 --> ASH");
+				startActivity(i);
+			}
+		}
+		break;
+		
+		case (R.id.button2): {
+			if (serverMSG == "") {
+				Toast.makeText(WelcomeScreen.this, "Enter Server Address 3!",
+						Toast.LENGTH_LONG).show();
+				break;
+			} else if(serverMSG != "") {
+				
+				i.putExtra(EXTRA_MESSAGE, "2");
+				i.putExtra(EXTRA_MESSAGE, serverMSG);
+				Log.d(TAG, "Chose Button 2 --> JOSH");
+				startActivity(i);
+				
+			}
+		}
+		break;
+		
+		}
+		
+		Toast.makeText(WelcomeScreen.this, String.valueOf(view.getId()),
+				Toast.LENGTH_LONG).show();
+		
+
+	}
+
 }
